dc27c3ebf3b13d39a15d847f30c7d87c26b60dc0
Alex Shen <as5gd@viginia.edu>
2020-03-27 14:30:32 -0400

    Updates

---
+++ precache-manifest.1eeebc75f6e714a79af4993751cc1d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a99f4db2fe13f9dd8c8a5585b4dbbc",
+    "url": "/Neuralize/index.html"
+  },
+  {
+    "revision": "af7e6401c50260d5856f",
+    "url": "/Neuralize/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08e698eed470a3282a72",
+    "url": "/Neuralize/static/js/2.315677d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Neuralize/static/js/2.315677d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7e6401c50260d5856f",
+    "url": "/Neuralize/static/js/main.fdcfd7b1.chunk.js"
+  },
+  {
+    "revision": "c1fea023b4fb4971dc73",
+    "url": "/Neuralize/static/js/runtime-main.3028b935.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Neuralize/static/media/logo.5d5d9eef.svg"
+  }
+]);
