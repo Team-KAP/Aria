e78061b3bf3c4f4ab7922c9f7d9dc6f6cc1c28df
Alex Shen <shen.alexander@gmail.com>
2020-03-28 14:03:59 -0400

    Updates

---
+++ precache-manifest.bd8548da1335e6601defc6fce188e35f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b3eed496ef3fdd0ea59ebfb6cf1ad3",
+    "url": "/Neuralize/index.html"
+  },
+  {
+    "revision": "29a1679b6c5468d44e64",
+    "url": "/Neuralize/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "08e698eed470a3282a72",
+    "url": "/Neuralize/static/js/2.315677d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Neuralize/static/js/2.315677d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a1679b6c5468d44e64",
+    "url": "/Neuralize/static/js/main.fa4469e5.chunk.js"
+  },
+  {
+    "revision": "c1fea023b4fb4971dc73",
+    "url": "/Neuralize/static/js/runtime-main.3028b935.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Neuralize/static/media/logo.ee7cd8ed.svg"
+  }
+]);
