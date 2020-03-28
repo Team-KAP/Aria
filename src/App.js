915b6da29d4e2ddedb8b682bb23e695355e300ce
Alex Shen <shen.alexander@gmail.com>
2020-03-28 14:37:53 -0400

    Palash's work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+class Layer {
+  constructor(num_nodes, activation, nextLayer, prevLayer){
+    num_nodes = 64; //temporary
+    activation = "relu"; //default
+    nextLayer = null;
+    prevLayer = null;
+  }
+}
+
+class network {
+  constructor(inputLayers, hiddenLayers, outputLayers) {
+
+  }
+}
 
 function App() {
+  let kerasCode = new Map();
+  kerasCode.set("model", "model = Sequential()");
+  kerasCode.set("relu", ", activation="relu"") 
+  kerasCode.set("end", "))");
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          OMEGALUL
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
