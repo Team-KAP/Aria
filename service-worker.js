5a7dc99d8d9a480f349771bedf9b776b3ffb44eb
Alex Shen <shen.alexander@gmail.com>
2020-03-30 17:18:51 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NetWeaver/precache-manifest.0e19ca7cccb5a9d4758b6b3f24fd8d45.js"
+  "/NetWeaver/precache-manifest.bedd1ee05837d1e3c00737275245ac67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NetWeaver/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
